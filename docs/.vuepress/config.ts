f49bcc20d074362c321c0b3333914801989fc32f
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-07-03 21:28:45 +0900

    chore: add new deploye tool

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from 'vuepress'
-// import { defaultTheme } from '@vuepress/theme-default'
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   base: '/nyabb-document/',
@@ -8,7 +8,7 @@ export default defineUserConfig({
   description: 'NyaBB Document',
   port: 10131,
 
-  // theme: defaultTheme({
-  //   navbar: [{ text: '开发笔记', link: '/develop-note' }],
-  // }),
+  theme: defaultTheme({
+    navbar: [{ text: '开发笔记', link: '/develop-note' }],
+  }),
 })
