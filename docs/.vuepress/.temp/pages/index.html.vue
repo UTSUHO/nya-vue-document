99a6b8b590233d5dfb5d4182da566cbefc2f126a
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-07-03 19:49:00 +0900

    fix: gitignore

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,3 +0,0 @@
-<template><div><h1 id="nyabb-document" tabindex="-1"><a class="header-anchor" href="#nyabb-document" aria-hidden="true">#</a> NyaBB Document</h1>
-<h2 id="sample" tabindex="-1"><a class="header-anchor" href="#sample" aria-hidden="true">#</a> SAMPLE</h2>
-</div></template>
